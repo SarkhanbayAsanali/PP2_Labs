94cb8b6802a1ee9ce2a3fc5ab817be679477e381
SarkhanbayAsanali <a_sarkhanbay@kbtu.kz>
2024-01-30 10:38:50 +0600

    lab1

---
+++ Examples_casting.py
@@ -0,0 +1,41 @@
+#Example 1:
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
+
+Output:
+1
+2
+3
+
+#Example 2:
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float("4.2")
+print(x)
+print(y)
+print(z)
+print(w)
+
+Output:
+1.0
+2.8
+3.0
+4.2
+
+#Example 3:
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
+
+Output:
+s1
+2
+3.0
---
+++ Examples_numbers.py
@@ -0,0 +1,69 @@
+#Example 1:
+x = 1
+y = 2.8
+z = 1j
+
+print (type(x))
+print (type(y))
+print (type(z))
+
+Output: 
+<class 'int'>
+<class 'float'>
+<class 'complex'>
+
+#Example 2:
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print (type(x))
+print (type(y))
+print (type(z))
+
+Output: 
+<class 'int'>
+<class 'int'>
+<class 'int'>
+
+#Example 3:
+x = 1.10
+y = 1.0
+z = -35.59
+
+print (type(x))
+print (type(y))
+print (type(z))
+
+Output:
+<class 'float'>
+<class 'float'>
+<class 'float'>
+
+#Example 4:
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print (type(x))
+print (type(y))
+print (type(z))
+
+Output:
+<class 'float'>
+<class 'float'>
+<class 'float'>
+
+#Example 5:
+x = 3+5j
+y = 5j
+z = -5j
+
+print (type(x))
+print (type(y))
+print (type(z))
+
+Output:
+<class 'complex'>
+<class 'complex'>
+<class 'complex'>
