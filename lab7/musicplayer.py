8b48a418ec42576554a8f6712477d47cbb3c6595
SarkhanbayAsanali <a_sarkhanbay@kbtu.kz>
2024-04-11 01:44:27 +0600

    lab7

---
+++ lab7/musicplayer.py
@@ -0,0 +1,35 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 480))
+
+pygame.display.set_caption("Music Player")
+
+pygame.mixer.music.load("song.mp3")
+
+pygame.mixer.music.play()
+
+
+list = ["song.mp3", "song1.mp3"]
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_d:
+                pygame.mixer.music.unpause()
+            elif event.key == pygame.K_s:
+                pygame.mixer.music.stop()
+            elif event.key == pygame.K_n:
+                for i in range(len()):
+                    pygame.mixer_music.load("")
+            elif event.key == pygame.K_b:
+                pygame.mixer.music.load("song.mp3")
+                pygame.mixer.music.play()
+            elif event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                quit()
+
+    pygame.display.update()
